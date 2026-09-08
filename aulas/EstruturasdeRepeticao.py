98eb34af09e0ee8782d46190d1d6fa4825645458
Bruno <bruno.kise@aluno.senai.br>
2026-09-08 20:01:29 -0300

    EstruturasdeRepeticao.py

--- aulas/EstruturasdeRepeticao.py
+++ aulas/EstruturasdeRepeticao.py
@@ -1,8 +1,38 @@
 # REPETIÇÃO WHILE  ->  Enquanto
 
-ano_nascimento = 2004
-ano_final = 2077
+#EXEMPLO 1: NÃO RODAR, POIS TRAVA O PROGRAMA!!!
+#ano_nascimento = 2004
+#ano_final = 2077
 
-idade = 0
-while ano_nascimento <= ano_final:
-    idade += 1
+#idade = 0
+#while ano_nascimento <= ano_final:    (ou while True:)
+#    idade += 1
+#    print(idade)
+#    break (PARA O PROGRAMA)
+
+"------------------------------------------------------------------------"
+#EXEMPLO 2:
+
+#ano_nascimento = 2004
+#ano_final = 2077
+
+#print("O ano atual é: ", ano_nascimento)
+
+#idade = 0
+#while ano_nascimento <= ano_final:
+#    idade += 1
+#    print(idade)
+
+#    if (ano_nascimento == ano_final):
+#        print("O ano atual é: ", ano_nascimento)
+#    ano_nascimento += 1
+
+
+"-------------------------------------------------------------------------"
+
+seu_nome = input("Digite seu nome: ")
+
+while seu_nome != "Bruno":
+    print("Pessoa não encontrada..")
+    seu_nome = input("Digite seu nome novamente: ")
+print("Bem vindo, Bruno")
