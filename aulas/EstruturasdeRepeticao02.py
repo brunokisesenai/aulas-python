80ce8db4cc86d97c57d18912fc0a3ab9f90d18a0
Bruno <bruno.kise@aluno.senai.br>
2026-09-09 20:17:24 -0300

    Estruturas de Repetição 2 (for)

---
+++ aulas/EstruturasdeRepeticao02.py
@@ -0,0 +1,23 @@
+# REPETIÇÃO FOR (para)
+
+#item (variável) objeto, função, lista
+#numero_digitado = int(input("Digite um número: "))
+#for numero_secreto in range (1, 11):
+
+#    if numero_digitado == numero_secreto:
+#        print("Você ganhou!")
+
+
+"-------------------------------------------------------------------------"
+
+#criando uma lista
+#                      0         1         2         3
+lista_de_alunos = ["aluno1", "aluno2", "aluno3", "aluno4"]
+print(lista_de_alunos)
+print(lista_de_alunos[0]) #print da posição '0' da lista
+
+print(type(lista_de_alunos))
+print(type(lista_de_alunos[0]))
+
+for valor in lista_de_alunos:
+    print(valor)
