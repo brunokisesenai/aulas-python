922832cc249c6733a0871f6df45eb81600d820f9
Bruno <bruno.kise@aluno.senai.br>
2026-09-11 20:56:05 -0300

    Aula Funções

---
+++ aulas/Funcoes.py
@@ -0,0 +1,48 @@
+# CONHECENDO FUNÇÕES NO PYTHON
+
+#EXEMPLO
+
+#numero1 = int(input("Digite um numero: "))
+#numero2 = int(input("Digite outro numero: "))
+#soma = numero1 + numero2
+
+#print(soma)
+
+"-----------------------------------------------------------------------"
+
+
+def soma():
+    print("Fazendo uma soma.")
+    numero1 = int(input("Número 1: "))
+    numero2 = int(input("Número 2: "))
+    return print(numero1 + numero2)
+
+soma() #"invoca" a definição de soma
+
+"----------------------------------------------------------"
+
+
+def subtracao():
+    print("Fazendo uma soma.")
+    numero1 = int(input("Número 1: "))
+    numero2 = int(input("Número 2: "))
+    return print(numero1 - numero2)
+
+subtracao() #"invoca" a definição de subtracao
+
+"----------------------------------------------------------------"
+
+#Teoria 1
+
+#def subtracao():
+#    while True:
+#        while True:
+#            while True:
+#                return #encerra o codigo
+
+"-------------------------------------------------------------------------"
+
+
+
+
+
---
+++ aulas/TesteFuncao.py
@@ -0,0 +1,3 @@
+#ARQUIVO TesteFuncao.py
+
+soma()
