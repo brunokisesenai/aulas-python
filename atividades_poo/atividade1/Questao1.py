23c31d1665e4fbdd119527e8f55cde91b673fc8f
Bruno <bruno.kise@aluno.senai.br>
2026-09-15 21:46:47 -0300

    atividade 1 POO

--- atividades_poo/atividade1/Questao1.py
+++ atividades_poo/atividade1/Questao1.py
@@ -7,7 +7,7 @@
 
 class Pessoa:
 
-    def __init__(self, nome, idade, peso, altura, profissao):
+    def __init__(self, nome, idade, peso):
         self.nome = nome
         self.idade = idade
         self.peso = peso
@@ -23,5 +23,13 @@ class Pessoa:
         print(self.peso)
 
 
+pessoa1 = Pessoa(nome = "Bruno", idade = 44, peso = 90)
+pessoa2 = Pessoa(nome = "Carlos", idade = 30, peso = 64)
+pessoa3 = Pessoa(nome = "Daniel", idade = 25, peso = 72)
+pessoa4 = Pessoa(nome = "Eduardo", idade = 16, peso = 81)
+pessoa5 = Pessoa(nome = "Fernando", idade = 60, peso = 59)
 
+print(pessoa1.nome_aluno(), pessoa1.idade_aluno(), pessoa1.peso_aluno())
+print(pessoa2.nome_aluno(), pessoa2.idade_aluno(), pessoa2.peso_aluno())
+print(pessoa3.nome_aluno(), pessoa3.idade_aluno(), pessoa3.peso_aluno())
 
