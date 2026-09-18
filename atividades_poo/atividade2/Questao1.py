0e91adac733c70136244147f63ef0d9d420c5e4a
Bruno <bruno.kise@aluno.senai.br>
2026-09-17 21:35:05 -0300

    Atividade 2 poo

--- atividades_poo/atividade2/Questao1.py
+++ atividades_poo/atividade2/Questao1.py
@@ -6,17 +6,35 @@ class Produto:
         self.__preco = preco
         self.__quantidade_estoque = quantidade_estoque
 
-    def adicionar_estoque(self, quantidade_estoque):
+
+    def adicionar_estoque(self, __quantidade_estoque):
+        estoque_adicionado = int(input("Digite a quantidade de produtos a serem adicionados: "))
+        estoque = estoque_adicionado + __quantidade_estoque
+        print(f"Foram adicionados {estoque_adicionado} unidades do produto ao estoque. O saldo atual é de {estoque} unidades.")
 
 
+    def realizar_venda(self, __quantidade_estoque):
+        venda = int(input("Digite a quantidade de produtos vendidos: "))
+        estoque_venda = __quantidade_estoque - venda
+        print(f"Foram vendidos {estoque_venda} unidades do produto. O saldo atual é de {estoque_venda} unidades.")
 
-    def realizar_venda(self, realizar_venda):
 
-
-
-    def aplicar_desconto(self, aplicar_desconto):
-
+    def aplicar_desconto(self, desconto, __preço):
+        desconto = int(input("Digite o valor do desconto aplicado ao produto: "))
+        valor_venda = __preço - desconto
+        print(f"O valor do final do produto foi {valor_venda}")
 
 
     def exibir_resumo(self, resumo):
+        self.resumo = resumo
+        print(resumo)
+
+
+produto_dados = Produto("Notebook", "5000", 100)
+print(produto_dados.nome)
+print(produto_dados.preco)
+print(produto_dados.quantidade_estoque)
+
+
+
 
