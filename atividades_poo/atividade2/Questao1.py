a1354143a5bc00489d13350cebcadeedcbb532ba
Bruno <bruno.kise@aluno.senai.br>
2026-09-18 20:30:01 -0300

    Atividade 2 poo

--- atividades_poo/atividade2/Questao1.py
+++ atividades_poo/atividade2/Questao1.py
@@ -9,7 +9,6 @@ class Produto:
         self.__quantidade_estoque = quantidade_estoque
 
 
-
 #MÉTODOS:
 
     def adicionar_estoque(self, quantidade):
@@ -23,18 +22,19 @@ class Produto:
         if venda <= self.__quantidade_estoque:
             self.__quantidade_estoque -= venda
             print(f'Quantidade retirada: {venda}')
-            print(f'Estoque restante: {self.__quantidade_estoque()}')
+            print(f'Estoque restante: {self.__quantidade_estoque}')
         else:
-            print(f'Estoque atual: {self.__quantidade_estoque()}')
-            print(f"Estoque {venda}")
-            print("Estoque insuficiente")
+            print(f'Estoque atual: {self.__quantidade_estoque}')
+            print(f"Tentativa de retirada de {venda} unidades. Estoque insuficiente!!!")
 
 
+    def aplicar_desconto(self, desconto):
+        if desconto > 0:
+            self.__preco -= desconto
+            print(f"O valor final do produto com desconto de R$ {desconto}, foi de R$ {self.__preco}")
 
-    def aplicar_desconto(self, desconto, __preço):
-        desconto = int(input("Digite o valor do desconto aplicado ao produto: "))
-        valor_venda = __preço - desconto
-        print(f"O valor do final do produto foi {valor_venda}")
+        else:
+            print("Desconto inválido")
 
 
     def exibir_resumo(self, resumo):
@@ -43,11 +43,26 @@ class Produto:
 
 
 
-meu_produto = Produto("Notebook", "5000", 50)
-print(meu_produto.__dict__)
-meu_produto.adicionar_estoque(-50)
-print(meu_produto.__dict__)
+meu_produto = Produto("Notebook", 5000, 50)
 
 
+#ADICIONAR PRODUTO
+#print(meu_produto.__dict__)
+#meu_produto.adicionar_estoque(-50)
+#print(meu_produto.__dict__)
+
+#meu_produto.__quantidade_estoque = -50
+#meu_produto.__preco = -100
+#print(meu_produto.__dict__)
+
+#REALIZAR VENDA
+#meu_produto.realizar_venda(10)
+#meu_produto.realizar_venda(100)
+
+#DESCONTO:
+#meu_produto.aplicar_desconto(50)
+
+#EXIBIR RESUMO:
+#print(meu_produto.__dict__)
 
 
