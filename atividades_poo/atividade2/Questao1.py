49d6fb064a51b31569342f8e52ef3fc1a5139d77
Bruno <bruno.kise@aluno.senai.br>
2026-09-17 21:16:05 -0300

    Atividade 2 poo

---
+++ atividades_poo/atividade2/Questao1.py
@@ -0,0 +1,22 @@
+#  QUESTÃO 1
+
+class Produto:
+    def __init__(self, nome, preco, quantidade_estoque):
+        self.__nome = nome
+        self.__preco = preco
+        self.__quantidade_estoque = quantidade_estoque
+
+    def adicionar_estoque(self, quantidade_estoque):
+
+
+
+    def realizar_venda(self, realizar_venda):
+
+
+
+    def aplicar_desconto(self, aplicar_desconto):
+
+
+
+    def exibir_resumo(self, resumo):
+
