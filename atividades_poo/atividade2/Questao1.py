7ed9af78f619decef5154b5b1a4f749c3134f4cb
Bruno <bruno.kise@aluno.senai.br>
2026-09-18 20:50:42 -0300

    Atividade 2 poo

--- atividades_poo/atividade2/Questao1.py
+++ atividades_poo/atividade2/Questao1.py
@@ -14,6 +14,7 @@ class Produto:
     def adicionar_estoque(self, quantidade):
         if quantidade > 0:
             self.__quantidade_estoque += quantidade
+            print(f"Foram adicionados {quantidade} unidades ao estoque. O estoque atual é de {self.__quantidade_estoque} unidades.")
         else:
             print("Erro: quantidade inválida!")
 
@@ -25,21 +26,20 @@ class Produto:
             print(f'Estoque restante: {self.__quantidade_estoque}')
         else:
             print(f'Estoque atual: {self.__quantidade_estoque}')
-            print(f"Tentativa de retirada de {venda} unidades. Estoque insuficiente!!!")
+            print(f"Tentativa de retirada de {venda} unidades. Venda negada. Saldo insuficiente.!!!")
 
 
     def aplicar_desconto(self, desconto):
-        if desconto > 0:
-            self.__preco -= desconto
-            print(f"O valor final do produto com desconto de R$ {desconto}, foi de R$ {self.__preco}")
-
+        if desconto > 0 and desconto <= 80:
+            self.__preco *= (desconto/100)
+            print(f"O valor final do produto com desconto de {desconto} %, foi de R$ {self.__preco}")
         else:
-            print("Desconto inválido")
+            print(f"Erro: Desconto inválido!. Valor do produto: R$ {self.__preco}")
 
 
     def exibir_resumo(self, resumo):
         self.resumo = resumo
-        print(resumo)
+        print(f"Resumo: existem {self.__quantidade_estoque} unidades do produto {self.__nome} com valor de R$ {self.__preco} no estoque.")
 
 
 
@@ -48,9 +48,9 @@ meu_produto = Produto("Notebook", 5000, 50)
 
 #ADICIONAR PRODUTO
 #print(meu_produto.__dict__)
-#meu_produto.adicionar_estoque(-50)
+#meu_produto.adicionar_estoque(50)
 #print(meu_produto.__dict__)
-
+#meu_produto.adicionar_estoque(-50)
 #meu_produto.__quantidade_estoque = -50
 #meu_produto.__preco = -100
 #print(meu_produto.__dict__)
@@ -60,9 +60,9 @@ meu_produto = Produto("Notebook", 5000, 50)
 #meu_produto.realizar_venda(100)
 
 #DESCONTO:
-#meu_produto.aplicar_desconto(50)
+#meu_produto.aplicar_desconto(90)
 
 #EXIBIR RESUMO:
-#print(meu_produto.__dict__)
+meu_produto.exibir_resumo(0)
 
 
