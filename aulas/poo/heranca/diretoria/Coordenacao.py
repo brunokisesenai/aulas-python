11ea79b8f92db24376c6b2ffba6facc1fdd52f6f
Bruno <bruno.kise@aluno.senai.br>
2026-09-18 21:52:10 -0300

    Heranca

--- aulas/poo/heranca/diretoria/Coordenacao.py
+++ aulas/poo/heranca/diretoria/Coordenacao.py
@@ -1,2 +1,25 @@
 class Coordenacao:  #Classe PAI
-    def __init__(self, ):
+    def __init__(self, professores, cursos, alunos):
+        self.__professores = professores
+        self.__cursos = cursos
+        self.__alunos = alunos
+
+
+    @property
+    def cursos(self):
+        return self.__cursos
+
+    @property
+    def professores(self):
+        return self.__professores
+
+    @property
+    def alunos(self):
+        return self.__alunos
+
+
+    def escolher_professores(self, index_professor):
+        return self.__professores[index_professor]
+
+
+
