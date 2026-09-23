67578567d34b6c043defb8ebc2b7d51188fc426b
Bruno <bruno.kise@aluno.senai.br>
2026-09-22 21:49:53 -0300

    Atividade POO 4

--- atividades_poo/atividade4/Navio.py
+++ atividades_poo/atividade4/Navio.py
@@ -4,14 +4,17 @@ from CalculoDeFrete import CalculoDeFrete
 
 class Navio(CalculoDeFrete):
     def iniciando(self):
-        print(f"O valor do frete será calculado baseado no peso e distância")
+        print(f"O valor da taxa será calculado baseado na distância e peso")
 
     def calcular_taxa(self, distancia, valor, prazo):
-        print(f"O Valor do frete do caminhão é de R$ {valor} por km")
+        print(f"O Valor da taxa do navio é de R$ {valor} por km")
         self.distancia = distancia
         self.valor = self.distancia * 20
+        self.prazo = prazo
         print(f"O valor do frete para {self.distancia} km é de R$ {valor}.")
 
 
+
 taxa = Navio()
-taxa.calcular_taxa(50, 100, 30)
+taxa.iniciando()
+taxa.calcular_taxa(50, 100, 70)
